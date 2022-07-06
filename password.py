0f51f53df3f3ad6850a04695ad92852374ba4fa2
dineshtadepalli12 <dineshtadepalli12@gmail.com>
2022-07-05 19:31:28 -0500

    Basic modes have been created

---
+++ password.py
@@ -0,0 +1,15 @@
+pwd = input("What is the master password ")
+
+while True:
+
+    mode = input("would you like to add/view password or quit ")
+    if mode =="q":
+        break
+    if mode =="add":
+        pass
+    elif mode =="view":
+        pass
+    else:
+        print("invalid mode")
+        continue
+print("Thank you, see you next time")
